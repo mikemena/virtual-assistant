e70598c5a62d7502d3791c14f9fc7c1305d2516d
tokyo <tokyolabs@protonmail.com>
2024-01-28 19:09:34 -0500

    wip

--- text_to_voice.py
+++ text_to_voice.py
@@ -1,12 +1,18 @@
+# import pyttsx3
+
+
+# def speak(message):
+#     engine = pyttsx3.init()
+
+#     engine.say(message)
+
+#     engine.runAndWait()
+
+
+# speak("Hello Cooper")
+
 import pyttsx3
 
-
-def speak(message):
-    engine = pyttsx3.init()
-
-    engine.say(message)
-
-    engine.runAndWait()
-
-
-speak("Hello Cooper")
+engine = pyttsx3.init()
+engine.say("I will speak this text")
+engine.runAndWait()
