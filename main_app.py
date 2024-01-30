8e31f7a58dc51b55b5bad548158d87a949108c08
tokyo <tokyolabs@protonmail.com>
2024-01-30 18:38:44 -0500

    wip

--- main_app.py
+++ main_app.py
@@ -2,6 +2,7 @@ import voice_to_text as vt
 import text_to_voice as tv
 import webbrowser
 import pyjokes
+import yfinance as yf
 
 
 def my_assistant():
@@ -10,7 +11,7 @@ def my_assistant():
     go_on = True
     while go_on:
         # Record audio
-        recorded_audio = vt.record_audio(duration=5)
+        recorded_audio = vt.record_audio(duration=7)
         print("recorded_audio type -> ", type(recorded_audio))
 
         # Convert the NumPy array to raw audio bytes
@@ -21,7 +22,7 @@ def my_assistant():
         my_request = vt.transform_audio_into_text(audio_bytes)
         print(my_request)
         if my_request:
-            if "what is today" in my_request:
+            if "what day is today" in my_request:
                 day = tv.ask_day()
                 tv.speak(f"Today is {day}")
                 continue
@@ -40,6 +41,18 @@ def my_assistant():
             elif "joke" in my_request:
                 tv.speak(pyjokes.get_joke())
                 continue
+            elif "stock price" in my_request:
+                share = my_request.split()[-2].strip()
+                portfolio = {"apple": "APPL", "amazon": "AMZN", "google": "GOOGL"}
+                try:
+                    searched_stock = portfolio[share]
+                    searched_stock = yf.Ticker(searched_stock)
+                    price = searched_stock.info["regularMarketPrice"]
+                    tv.speak(f"I found it! The price of {share} is {price}")
+                    continue
+                except Exception as e:
+                    tv.speak(f"I am sorry, but I didn´t find it. The error is {e}")
+                    continue
             elif "goodbye" in my_request:
                 tv.speak("I am going to rest. Let me know if you need anything.")
                 go_on = False
